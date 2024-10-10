bdfb72361f1118bcd5a12f9cdd942e5003620ba3
ashik <mdashikash18@gmail.com>
2024-10-10 09:58:43 +0000

    Solution

--- src/components/FruitsCounter/index.js
+++ src/components/FruitsCounter/index.js
@@ -3,36 +3,50 @@ import {Component} from 'react'
 
 import './index.css'
 
+class FruitsCounter extends Component {
+  state = {mango: 0, banana: 0}
 
-class FruitsCounter extends Component{
-    state = {mango: 0, banana: 0}
-
-    onMango = () => {
-    this.setState((prevState) => ({ mango: prevState.mango + 1 }))
+  onMango = () => {
+    this.setState(prevState => ({mango: prevState.mango + 1}))
   }
-     onMango = () => {
-    this.setState((prevState) => ({ banana: prevState.banana + 1 }))
+  onBanana = () => {
+    this.setState(prevState => ({banana: prevState.banana + 1}))
   }
 
-  render(){
-    const { mango, banana } = this.state
-    return(
-        <div class="main-container">
-          <h1>Bob ate {mango} mangoes {banana} bananas</h1>
-          <div>
-            <div className="mango-container">
-              <img src="https://assets.ccbp.in/frontend/react-js/mango-img.png"/>
-              <button>Eat Mango</button>
+  render() {
+    const {mango, banana} = this.state
+    return (
+      <div className="card-inside-container">
+        <h1 className="heading">
+          Bob ate {mango} mangoes {banana} bananas
+        </h1>
+        <div className="fruits-container">
+          <div className="fruit-card-container mango-container">
+            <div>
+              <img
+                className="fruit-image"
+                src="https://assets.ccbp.in/frontend/react-js/mango-img.png" alt="mango"
+              />
             </div>
-            <div className="banana-container">
-              <img src="https://assets.ccbp.in/frontend/react-js/banana-img.png"/>
-              <button>Eat Banana</button>
+            <button className="eat-btn" onClick={this.onMango}>
+              Eat Mango
+            </button>
+          </div>
+          <div className="fruit-card-container">
+            <div>
+              <img
+                className="fruit-image"
+                src="https://assets.ccbp.in/frontend/react-js/banana-img.png" alt="banana"
+              />
             </div>
+            <button className="eat-btn" onClick={this.onBanana}>
+              Eat Banana
+            </button>
           </div>
         </div>
+      </div>
     )
   }
 }
 
-
-export default FruitsCounter;
+export default FruitsCounter
